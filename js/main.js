3b126307de52ce258ff3b2d08dfad37c3ccb6179
Jacob Shaeffer <shaeffer.jacob@gmail.com>
2017-12-16 00:25:39 -0700

    So far this is pretty cool

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
-var navLinks;
-var sections;
 
 function onScroll(){
     
-    var scrollTop = $(window).scrollTop();
-    for(var i=0; i<sections.length; i++){
-        if($(sections[i]).offset().top <= (scrollTop + ($(window).height() * 0.25))){
-            navLinks.removeClass("active");
-            $('a[href="#' + sections[i].id + '"]').parent().addClass("active");
-        }
-    }
 }
 
 function main(){
-    navLinks = $("ul.nav-bar-links > li");
-    sections = $("div.section");
-
     $(window).scroll(
         $.throttle(10, () => {
             onScroll();
