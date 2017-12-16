75853ac05e1f522054bbc431afe7d978b2840f5e
Jacob Shaeffer <shaeffer.jacob@gmail.com>
2017-12-16 15:50:50 -0700

    it's geting cooler, time for the jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
+var sectionTitles;
+
 
 function onScroll(){
-    
+    for(var i=0; i<sectionTitles.length; i++) {
+        if($(sectionTitles[i]).offset().top >= $(window).scrollTop()){
+            $(sectionTitles[i]).addClass("section-title").end().removeClass("section-title-fixed");
+        }
+    }
 }
 
 function main(){
+    sectionTitles = $(".section-title-fixed");
+    sections = $('section');
+
     $(window).scroll(
         $.throttle(10, () => {
             onScroll();
