53002ba1f44fabb3e45e0d8f64274219d076a0e4
Jacob Shaeffer <jshaeffer@azmag.gov>
2017-12-20 11:14:10 -0700

    removed hard coding and bettered animation
    a lot of the hard coding was removed meaning scaling this will be
    significantly easlier in the future. The code also looks much nicer now.
    
    Changes some of the code around he titles attaching and detaching so
    the titles now smoothly move back and forth from fixed to unfixed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 var sectionTitles;
 var botValues = [1.81, 1.47, 0.83, 0];
-var fontValues = [0,0,0,0];//[ 1, 0.66, 0.44, 0.293 ];
-//var bottoms = [ 66, 35, 14, 0 ];
-var catagories;
+var fontValues = [];
+var isFixed = [];
+var movingTitles;
 
 function setup(){
     sectionTitles = $(".section-title");
-    sections = $(".section");
-    catagories = [ "me", "projects", "education", "employment" ];
+    movingTitles = $(".moving-title");
 
-    //$("#test").on('click', function() { $("HTML, BODY").animate({ scrollTop: 0 }, 500); });
-    for(var i=0; i<catagories.length; i++){
-        let top = $("#" + catagories[i]).offset().top;
-        $('a[href="#' + catagories[i] +'"]').on('click', function(e){
+    for(var i=0; i<movingTitles.length; i++){
+        let top = $(movingTitles[i]).offset().top;
+        console.log('a[href="#mt_' + i +'"]');
+        $('a[href="#mt_' + i +'"]').on('click', function(e){
+            console.log("click");
             e.preventDefault();
             $("HTML, BODY").animate({scrollTop: top}, 750);
         })
@@ -22,59 +22,79 @@ function setup(){
         var title = sectionTitles[i];
         $(title).removeClass("section-title");
         $(title).addClass("section-title-fixed");
+        fontValues.push(0);// = new Array(catagories.length);
+        isFixed.push(true);// = new Array(catagories.length);
     }
 }
 
 function cssHasJavascript(){
-    var base = "#section-display-text-";
-    for(var i=0; i<catagories.length; i++){
-        $(base + catagories[i]).css("font-size", fontValues[i] + "em");
+    var base = $(".section-display-text");
+    for(var i=0; i<base.length; i++){
+        $(base[i]).css("font-size", fontValues[i] + "em");
     }
 }
 
 function unnammed(){
     var windowBot = $(window).scrollTop() + $(window).height();
-    
-    for(var i=0; i<sectionTitles.length; i++) {
-        var title = sectionTitles[i];
-        var top = $(title).parent().offset().top;
+    var recalculate = [];
+    var needsRecalulating = false;
 
-        var dynamicSpacing = (botValues[i] * 48) + $(title).height();//Fixme: Somthing wierd is going on here
-
-        //set the fonts of the titles as the page scrolls
-        var distance = top - windowBot;
+    for(var i=0; i<sectionTitles.length; i++){
+        var distance = $(sectionTitles[i]).parent().offset().top - windowBot;
         var truncated = distance > 0 ? distance : 0;
         var zeroOne = truncated * 0.85 / ($(document).height() * 3 / 4);
         fontValues[i] = 1 - zeroOne;
-        
-
-        if(top <= windowBot - dynamicSpacing){
-            if($(title).hasClass("section-title-fixed")){
-                $(title).addClass("section-title");
-                $(title).removeClass("section-title-fixed");
-                $(title).css({"bottom": "0px"});
-            }
-        }
-        else{
-            if($(title).hasClass("section-title")){
-                $(title).addClass("section-title-fixed");
-                $(title).removeClass("section-title");
-                $(title).css({"bottom": "1px"});
-            }
-        }
     }
 
     for(var i=0; i<sectionTitles.length; i++){
-        //console.log($(sectionTitles[i]).css("bottom"));
-        var title = $(sectionTitles[i]);
-        if($(sectionTitles[i]).css("bottom") != "0px"){
+        if(isFixed[i]){
             botValues[i] = 0;
             for(var j=3; j>i; j--){
                 botValues[i] += fontValues[j];
             }
             $(sectionTitles[i]).css("bottom", botValues[i] + "em");
         }
+        recalculate.push(false);
     }
+    
+    for(var i=0; i<sectionTitles.length; i++) {
+        var title = sectionTitles[i];
+        var top = $(title).parent().offset().top;
+
+        
+
+        var dynamicSpacing = (botValues[i] * 48) + $(title).height();
+        if(top <= windowBot - dynamicSpacing){
+            if($(title).hasClass("section-title-fixed")){
+                $(title).addClass("section-title");
+                $(title).removeClass("section-title-fixed");
+                $(title).css({"bottom": "0px"});
+                isFixed[i] = false;
+            }
+        }
+        else{
+            if($(title).hasClass("section-title")){
+                $(title).addClass("section-title-fixed");
+                $(title).removeClass("section-title");
+                isFixed[i] = true;
+                recalculate[i] = true;
+                needsRecalulating = true;
+            }
+        }
+    }
+
+    if(needsRecalulating){
+        for(var i=0; i<recalculate.length; i++){
+            if(recalculate[i]){
+                botValues[i] = 0;
+                for(var j=3; j>i; j--){
+                    botValues[i] += fontValues[j];
+                }
+                $(sectionTitles[i]).css("bottom", botValues[i] + "em");
+            }
+        }
+    }
+
     cssHasJavascript();
 }
 
